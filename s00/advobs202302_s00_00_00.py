8c09ef941f64fcbdc1f9aecd511e4b9181e78bd8
だいすけ <77160981+kinoshitadaisuke@users.noreply.github.com>
2023-02-16 13:18:40 +0800

    Add files via upload

---
+++ s00/advobs202302_s00_00_00.py
@@ -0,0 +1,8 @@
+#!/usr/pkg/bin/python3.9
+
+#
+# Time-stamp: <2023/02/16 13:17:26 (CST) daisuke>
+#
+
+# printing a sentence using function print ()
+print ("This is my very first Python script for this course.")
