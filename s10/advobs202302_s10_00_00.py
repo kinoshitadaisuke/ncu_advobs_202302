a44c37a332f925790ea25d6dd441423a7b212b21
だいすけ <77160981+kinoshitadaisuke@users.noreply.github.com>
2023-04-20 12:22:57 +0800

    Add files via upload

---
+++ s10/advobs202302_s10_00_00.py
@@ -0,0 +1,42 @@
+#!/usr/pkg/bin/python3.9
+
+#
+# Time-stamp: <2023/04/20 09:31:22 (CST) daisuke>
+#
+
+# importing urllib module
+import urllib.request
+
+# importing ssl module
+import ssl
+
+# allow insecure downloading
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# URL of data file
+url_data = 'https://s3b.astro.ncu.edu.tw/advobs_202302/data/data_s10.tar.xz'
+
+# output file name
+file_output = 'data_s10.tar.xz'
+
+# printing status
+print (f'Now, fetching the file {url_data}...')
+
+# opening URL
+with urllib.request.urlopen (url_data) as fh_read:
+    # reading data
+    data_byte = fh_read.read ()
+
+# printing status
+print (f'Finished fetching the file {url_data}!')
+
+# printing status
+print (f'Now, writing the data into file {file_output}...')
+
+# opening file for writing
+with open (file_output, 'wb') as fh_write:
+    # writing data
+    fh_write.write (data_byte)
+
+# printing status
+print (f'Finished writing the data into file {file_output}!')
